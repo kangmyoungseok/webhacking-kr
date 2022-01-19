0a7058b21fdea53ddc874d08b9f32cc0a83107f5
Myeongseokkang <kms00129@naver.com>
2022-01-19 16:27:11 +0900

    31번 문제 풀이 코드

---
+++ 31-socket/31-socket_server.py
@@ -0,0 +1,26 @@
+import socket
+
+HOST='0.0.0.0'
+PORT = 10000
+
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+
+server_socket.bind((HOST,PORT))
+server_socket.listen()
+
+client_socket, addr = server_socket.accept()
+
+print("connected by",addr)
+
+while True:
+    data = client_socket.recv(1024)
+    if not data:
+        break
+
+    print('Received From', addr,data.decode())
+    client_socket.sendall(data)
+
+client_socket.close()
+server_socket.close()
